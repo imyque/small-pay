5ec98c4672a43dd133c47004443a4491d1b61635
Michael Ikhane <imyque@iMyques-MBP-2.local>
2019-06-21 08:49:01 +0100

    New deploy method

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const port = process.env.PORT || 80;
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname, '/dist/'));
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname, '/dist/index.html')
+});
 app.listen(port);
+console.log("Server started on port " + port);
